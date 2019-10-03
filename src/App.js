c26816dcc0415a796ba60077b5b4883895b226ca
Claudia Lorien Calero <claudia.calero@ultimasoftware.com>
2019-10-03 13:45:48 +0200

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
 
+const ADD_USER = 'ADD_USER'
+const appReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_USER: {
+      return{
+        users: [...state.users, action.payload]
+      };
+    }
+  }
+};
+
 function App() {
+
+  const initialState = {
+    users: [
+      { name: 'alan', age: 30 },
+      { name: 'messi', age: 31 },
+    ]
+  };
+
+  const [state, dispatch] = useReducer(
+    appReducer,
+    initialState
+  );
+
+  const addUsers = () => {
+    const name = document.getElementById('form-name-input').value;
+    const age = document.getElementById('form-age-input').value;
+    const newUser = { name, age };
+    dispatch({
+      type: ADD_USER,
+      payload: newUser
+    })
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.users.map(user => (<div>{`${user.name} ${user.age}`}</div>))}
+      <div className="form">
+        <div className="form-name">
+          <label>name:</label>
+          <input id="form-name-input" />
+        </div>
+        <div className="form-age">
+          <label>age:</label>
+          <input id="form-age-input" />
+        </div>
+        <button onClick={() => addUsers()}>Add usser</button>
+      </div>
     </div>
   );
 }
